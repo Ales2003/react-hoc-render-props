d55561fbc49b6f549c594f4774d054fc3749a16c
aliaksandr vishneuski <aliaksandr_vishneuski@senla.eu>
2019-08-19 15:56:47 +0300

    created render props start classses

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import './App.css';
 import ClickCounter from './components/ClickCounter'
 import HoverCounter from './components/HoverCounter'
 
+import ClickCounterTwo from './components/ClickCounterTwo'
+import HoverCounterTwo from './components/HoverCounterTwo'
 
 class App extends Component {
-
   
   render (){
     return (
       <div className="App">
-        Hello!
+        Hello HOC!
         <hr/>
+        {/* HOC */}
         <ClickCounter name="Alex"/>
         <HoverCounter name="Alex"/>
+        <hr/>
+        {/* renderProps */  }
+        Hello renderProps!
+        <hr/>
+        <ClickCounterTwo />
+        <HoverCounterTwo />
       </div>
     );
   }
-  
 }
 
 export default App;
---
+++ src/components/HoverCounterTwo.js
@@ -0,0 +1,30 @@
+import React, { Component} from 'react'
+import withCounter from './withCounter'
+
+class HoverCounterTwo extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            count: 0
+        }
+    }
+    
+    incrementCount = () => {
+        this.setState(prevState => {
+            return {count: prevState.count + 1}
+        })
+
+    }
+
+    render() {
+        const{ count } = this.state
+
+        return (
+            <div>
+                <h2 onMouseOver = {this.incrementCount}> {this.state.name} Hovered {count} times </h2>
+            </div>
+        )
+    }
+}
+
+export default HoverCounterTwo
